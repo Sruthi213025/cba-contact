8cd23ad3f0260437b1b6c65fac1455e6d1c3be1d
Sruthi S <sruthisugan3025@gmail.com>
2021-04-16 12:27:49 +0530

    initial commit

---
+++ src/component-contact/contact.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './component-contact/contact.css';
+
+function Contact()
+{
+    
+    return(
+
+    <div class="sr">
+        <img class="sr0"    />
+        <h1 class="sr1" >Reach to us!</h1>
+        <p class="sr2" >Let’s connect to extend our opportunities</p>
+        <img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600"/>
+    </div>
+
+
+
+    
+    );
+}
+export default Contact;
---
+++ src/component-contact/contact2.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './component2-contact/contact2.css';
+
+function Contact1()
+{
+    
+    return(
+        
+<div class="src">
+    <form>
+    <input class="srcc" type="text"  name="name" placeholder="Name" />
+    <input class="src4"  type="text"  name="mobile Number" placeholder="Mobile Number"/>
+    <input class="src4"  type="text" id="Email" name="lastname" placeholder="Email" />
+    <input  class="src5" name="subject" placeholder="Message" />
+    <button class="src6" style="">Send Now</button>
+    </form>
+</div>
+    )
+}
+export default Contact1
