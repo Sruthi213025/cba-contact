51afc8b71a7cd02599e8f5961ba0c617d1dd7650
Sruthi S <sruthisugan3025@gmail.com>
2021-04-20 10:08:10 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Contact from './Components/Contact/contact.component';
+import Contact1 from './Components/Contact/contact2.component';
+
+
+function App() {
+  return (
+<div>
+<Contact/>
+  <Contact1/>,
+</div>
+  
+  );
+}
+
+export default App;
